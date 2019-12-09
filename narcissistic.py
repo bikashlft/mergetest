d87b9b380487cf2acf57616c4949b53f0c2c1381
Bikash <remembermebikash@gmail.com>
2019-12-09 16:11:41 -0600

    new narcissistic.py file added

---
+++ narcissistic.py
@@ -0,0 +1,24 @@
+#This is comment by user1
+def main():
+
+    for i in range(1,1000):
+        temp = i
+        power = len(str(i))
+        #print(power)
+        sum = 0
+        while (temp != 0):
+            rem = temp % 10
+            #print ("rem:",rem)
+            sum += rem ** power
+            #print ("sum:",sum)
+            temp = temp // 10
+            #print ("temp:",temp)
+        if (sum == i):
+            print(i, "is a narcissistic no.")
+
+
+
+
+
+if __name__ == '__main__':
+    main()
